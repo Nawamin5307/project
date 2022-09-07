37fb9488ead0f7d1be3794f522b199120d18297a
Nawamin5307 <nawamin.w@kkumail.com>
2022-09-07 21:42:12 +0700

    Hello Git 2

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.println("My name is Dew");
     }
 }
